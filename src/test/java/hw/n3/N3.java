d6a6ef28aa99ae7bf519bdb94a6b56d612f00bc1
lordstan1 <>
2022-09-21 02:10:18 +0300

    Add files

---
+++ src/test/java/hw/n3/N3.java
@@ -0,0 +1,17 @@
+package hw.n3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class N3 {
+
+    @Test
+    void N3(){
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    void N3_2(){
+        Assertions.assertTrue(3 > 1);
+    }
+}
